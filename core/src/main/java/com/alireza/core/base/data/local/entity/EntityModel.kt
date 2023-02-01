0d989924cb3fd1cac4af8764312b41811f38dcff
Alireza Khosrowabadi <khosroabadi.alireza@gmail.com>
2023-02-01 07:51:31 +0330

    base classes created

---
+++ core/src/main/java/com/alireza/core/base/data/local/entity/EntityModel.kt
@@ -0,0 +1,7 @@
+package com.alireza.core.base.data.local.entity
+
+
+/**
+ * Local database or cache DataModels
+ * */
+interface EntityModel
---
+++ core/src/main/java/com/alireza/core/base/data/param/ParamModel.kt
@@ -0,0 +1,7 @@
+package com.alireza.core.base.data.param
+
+
+/**
+ * Data model to pass data to usecasees or repositories or pass data as network queries
+ * */
+interface ParamModel
